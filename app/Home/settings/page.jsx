c3650ba6961369ef4b32dde4def139090aae29ba
youcef <youceftebbi@outlook.com>
2024-04-04 09:35:48 +0300

    new/

--- app/Home/settings/page.jsx
+++ app/Home/settings/page.jsx
@@ -5,12 +5,13 @@ import { useEffect, useState } from "react"
 
 const Settings=()=>{
     const [clubInformation,setClubInformation]=useState({courts:[]})
+    const [clubInformationOriginal,setClubInformationOriginal]=useState({courts:[]})
     useEffect(()=>{
         const getClubInfo=async()=>{
             const clubInfoRef=doc(db,'Club','GeneralInformation')
             const clubinfoData=await getDoc(clubInfoRef)
           setClubInformation(clubinfoData.data())
-    
+          setClubInformationOriginal(clubinfoData.data())
         }
 getClubInfo()
     },[])
@@ -18,6 +19,10 @@ getClubInfo()
         const newCourt = prompt('Enter the new court name:');
         if (newCourt) {
             setClubInformation((prev) => ({
+                ...prev,
+                courts: [...prev.courts, newCourt],
+              }));
+              setClubInformationOriginal((prev) => ({
                 ...prev,
                 courts: [...prev.courts, newCourt],
               }));
@@ -39,10 +44,26 @@ getClubInfo()
             ...prev,
             courts:updatedCourts,
           }));
+          setClubInformationOriginal((prev) => ({
+            ...prev,
+            courts:updatedCourts,
+          }));
           await updateDoc(doc(db,'Club','GeneralInformation'),{
             courts:arrayRemove(newCourt)
           })
       };
+      const handleInputChange = (e) => {
+        console.log(e.target.name);
+        setClubInformation(prevReservation => ({
+          ...prevReservation,
+          [e.target.name]: e.target.value,
+        }));
+      };
+      const submitChanges=async()=>{
+        await updateDoc(doc(db,'Club','GeneralInformation'),clubInformation)
+        setClubInformationOriginal(clubInformation)
+        alert("Changes Commited!")
+      }
 
 return(
     <>
@@ -81,19 +102,19 @@ return(
           <label for="club-name">
             <span class="text-sm text-gray-500">Club Name</span>
             <div class="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-              <input type="text" name='name' id="club-name" class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" value={clubInformation?.name} />
+              <input onChange={handleInputChange} type="text" name='name' id="club-name" class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" value={clubInformation?.name} />
             </div>
           </label>
           <label for="club-website">
             <span class="text-sm text-gray-500">Website</span>
             <div class="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-              <input type="website" id="club-website" class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"  />
+              <input  onChange={handleInputChange} name="website" type="website" id="club-website" class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"  />
             </div>
           </label>
           <label for="club-phone">
             <span class="text-sm text-gray-500">Phone</span>
             <div class="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-              <input type="number" id="club-phone" class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"  />
+              <input type="number"  onChange={handleInputChange} name="phone" id="club-phone" class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"  />
             </div>
           </label>
 
@@ -108,7 +129,7 @@ return(
           <div class="flex flex-col space-y-3 px-4 py-6 sm:px-10">
             <label class="block" for="address">
               <p class="text-sm">Address</p>
-              <input class="w-full rounded-md border py-2 px-2 bg-gray-50 outline-none ring-blue-600 focus:ring-1 bg-white" type="text" value={clubInformation?.address} />
+              <input   onChange={handleInputChange} name='address' class="w-full rounded-md border py-2 px-2 bg-gray-50 outline-none ring-blue-600 focus:ring-1 bg-white" type="text" value={clubInformation?.address} />
             </label>
            
           </div>
@@ -123,7 +144,7 @@ return(
         <div class="flex flex-col space-y-3 px-4 py-6 sm:px-10">
   <label class="block" for="description">
     <p class="text-sm">Description</p>
-    <textarea class="w-full rounded-md border py-2 px-2 bg-gray-50 outline-none ring-blue-600 focus:ring-1 bg-white" id="description" rows="4" value={clubInformation?.description}></textarea>
+    <textarea  name='description'  onChange={handleInputChange} class="w-full rounded-md border py-2 px-2 bg-gray-50 outline-none ring-blue-600 focus:ring-1 bg-white" id="description" rows="4" value={clubInformation?.description}></textarea>
   </label>
 </div>
         </div>
@@ -162,8 +183,52 @@ onClick={handleAddCourt}
   </div>
   </div>
   <hr class="mt-4 mb-8" />
+  <p class="py-2 text-xl font-semibold">Days and Hours</p>
+
+<div class="grid grid-cols-3 gap-3 flex items-center">
+    <label for="club-name">
+      <span class="text-sm text-gray-500">Club Name</span>
+      <div class="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+        <input onChange={handleInputChange} type="text" name='name' id="club-name" class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" value={clubInformation?.name} />
+      </div>
+    </label>
+    <label for="club-website">
+      <span class="text-sm text-gray-500">Website</span>
+      <div class="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+        <input  onChange={handleInputChange} name="website" type="website" id="club-website" class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"  />
+      </div>
+    </label>
+    <label for="club-phone">
+      <span class="text-sm text-gray-500">Phone</span>
+      <div class="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+        <input type="number"  onChange={handleInputChange} name="phone" id="club-phone" class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"  />
+      </div>
+    </label>
+
+  </div>
+
+
+  {clubInformationOriginal !=clubInformation && (
+                         <>
+                    <button
+                         
+                     onClick={submitChanges}
+                         className="button-blue  mt-5 mb-8"
+                       >
+                        submit Changes
+                       </button>
+                         <button
+                           onClick={()=> setClubInformation(clubInformationOriginal)}
+                     
+                           className="button-red font-bold mt-5 ml-5 border rounded-lg px-5 py-2 mt-8"
+                         >
+                          Cancel Changes
+                         </button>
+                         </> )}  
+
     </div>
 
+    
   </div>
 </div>
 
